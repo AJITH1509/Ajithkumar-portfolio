13676e8326d11e379283d109c9ac0d93588b1e2d
AJITH1509 <ajithmuthu799@gmail.com>
2023-04-14 11:53:48 +0530

    resume link added

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,11 @@ import astronaut from "../images/homeimage.png";
 import github from "../images/github.png";
 import linkedin from "../images/linkedin.png";
 
+function redirectToLink() {
+  window.location.href =
+    "https://drive.google.com/file/d/1gvJRZZ5cdF38X8ZxIciwdnFXmqiuOa9t/view";
+}
+
 export const Home = () => {
   return (
     <div className="intro">
@@ -13,7 +18,7 @@ export const Home = () => {
           <span>Hi, I am 👋</span>
           <span>AJITHKUMAR</span>
           <span>Full stack developer</span>
-          <Button id="i-btn" variant="outlined">
+          <Button onClick={redirectToLink} id="i-btn" variant="outlined">
             Resume
           </Button>
           <div className="i-icons">
