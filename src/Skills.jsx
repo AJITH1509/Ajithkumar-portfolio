acdd1cee02d13bf01a384ea849eb0ee103edb50a
Ajithkumar M <118879048+AJITH1509@users.noreply.github.com>
2024-04-10 21:56:22 +0530

    Update Skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -8,6 +8,18 @@ import { useContext } from "react";
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 
+const SkillCard = ({ imgSrc, alt, text, value }) => {
+  return (
+    <div data-aos="flip-left" id="skill-cards">
+      <img className="skills-image" src={imgSrc} alt={alt} />
+      <p>{text}</p>
+      <Box>
+        <LinearProgress color="success" variant="determinate" value={value} />
+      </Box>
+    </div>
+  );
+};
+
 export const Skills = () => {
   const skillsData = [
     {
@@ -15,57 +27,87 @@ export const Skills = () => {
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
       alt: "JavaScript",
       text: "JavaScript",
+      value: 80,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
       alt: "React",
       text: "React",
+      value: 90,
+    },
+    {
+      imgSrc:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angular/angular-original.svg",
+      alt: "Angular",
+      text: "Angular",
+      value: 70,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
       alt: "Redux",
       text: "Redux",
+      value: 70,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
       alt: "MongoDB",
       text: "MongoDB",
+      value: 80,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
       alt: "Git",
       text: "Git",
+      value: 60,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
       alt: "VS Code",
       text: "VS Code",
+      value: 90,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
       alt: "HTML5",
       text: "HTML5",
+      value: 90,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
       alt: "CSS3",
       text: "CSS3",
+      value: 80,
     },
     {
       imgSrc:
         "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
-      alt: "Material UI",
-      text: "Material UI",
+      alt: "MaterialUI",
+      text: "MaterialUI",
+      value: 90,
     },
-    // Add more skills here
+    {
+      imgSrc:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+      alt: "Node.js",
+      text: "Node.js",
+      value: 90,
+    },
+    {
+      imgSrc:
+        "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+      alt: "Express",
+      text: "Express",
+      value: 90,
+    },
   ];
+  
 
   const { styles } = useContext(ThemeContext);
   useEffect(() => {
@@ -74,137 +116,20 @@ export const Skills = () => {
   return (
     <div style={{ backgroundColor: styles }} className="skills-container">
       <div>
-        <h2
-          data-aos="fade-down"
-          data-aos-duration="500"
-          className="skill-heading"
-          // data-aos-delay="500"
-        >
+        <h2 data-aos="fade-down" data-aos-duration="500" className="skill-heading">
           Skills & Tools
         </h2>
       </div>
       <div className="skill-cards-container">
-        <div id="skill-cards" data-aos="flip-left" data-aos-delay="600">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
+        {skillsData.map((skill, index) => (
+          <SkillCard
+            key={index}
+            imgSrc={skill.imgSrc}
+            alt={skill.alt}
+            text={skill.text}
+            value={skill.value}
           />
-          <p>JavaScript</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={80} />
-          </Box>
-        </div>
-
-        <div data-aos="flip-left" data-aos-delay="700" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          />
-          <p>React</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1400" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angular/angular-original.svg"
-          />
-          <p>Angular</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={70} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="800" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
-          />
-          <p>Redux</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={70} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="900" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
-          />
-          <p>MongoDB</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={80} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1000" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-          />
-          <p>Git Hub</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={60} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1100" id="skill-cards">
-          <img
-            className="skills-image"
-            src="	https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
-          />
-          <p>VS Code</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1200" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-          />
-          <p>Html5</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1300" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-          />
-          <p>CSS3</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={80} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1400" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
-          />
-          <p>MaterialUI</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1400" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
-          />
-          <p>Node Js</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
-        <div data-aos="flip-left" data-aos-delay="1400" id="skill-cards">
-          <img
-            className="skills-image"
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
-          />
-          <p>Express</p>
-          <Box>
-            <LinearProgress color="success" variant="determinate" value={90} />
-          </Box>
-        </div>
+        ))}
       </div>
     </div>
   );
